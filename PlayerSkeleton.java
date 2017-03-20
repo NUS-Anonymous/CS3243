5b1ba1b74678f3f78e59b43fa6892cae6769afa2
DESKTOP-80P8UQS\Hai Long <hlong.mseh@gmail.com>
2017-03-20 10:50:30 +0800

    Make a working engine

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -1,12 +1,42 @@
 
 public class PlayerSkeleton {
 
-	//implement this function to have a working system
+	/**
+	 * We generate all possible moves for the current piece
+	 * Calculate the weighted heuristic value of the field after we make the move
+	 * 
+	 * Choose the move that has in the smallest heuristic value
+	 * 
+	 * @param s - current state
+	 * @param legalMoves
+	 * @return the best move based on the heuristic values
+	 */
 	public int pickMove(State s, int[][] legalMoves) {
-		System.out.println(s.makeMove(0, 2));
-		
-		return 0;
+		double min = Double.MAX_VALUE;
+		int bestMove = -1;
+		for (int i = 0; i < legalMoves.length; i++) {
+			NextState tempState = new NextState(s.getTurnNumber(), s.getField(), 
+														s.getNextPiece(), s.getTop());
+			tempState.makeMove(legalMoves[i][NextState.ORIENT], legalMoves[i][NextState.SLOT]);
+			double value = getWeightedHeuristic(tempState);
+			if (value < min) {
+				min = value;
+				bestMove = i;
+			}			
+		}	
+		return bestMove;
 	}
+	
+	/**
+	 * Get the weighted heuristic value of a NextState
+	 * 
+	 * @param s - next state
+	 * @return the weighted heuristic of next state
+	 */
+	private double getWeightedHeuristic(NextState s) {
+		return s.calculateMinHeightHeuristic();
+	}
+
 	
 	public static void main(String[] args) {
 		State s = new State();
