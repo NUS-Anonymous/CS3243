0b4723acf3706e7d94570dd4a00f009fd0f37cff
Hai Long <hai_long@u.nus.edu>
2017-04-04 22:22:41 +0800

    fix some values in Individual and refractor it

--- Individual.java
+++ Individual.java
@@ -5,15 +5,15 @@
  * The fitness function is number of rows cleared after the game.
  */
 public class Individual implements Runnable {
-    
+
     //the weight vector
     private static final int size = PlayerSkeleton.NUM_OF_HEURISTICS;
     private double[] weight = new double[size];
-    
+
     //the player and the fitness, after the game runs once.
     private PlayerSkeleton player;
     private int fitness = -1;
-    
+
     /**
      * Deep copy of the individual
      * 
@@ -28,7 +28,7 @@ public class Individual implements Runnable {
         }
         return replica;
     }
-    
+
     /**
      * Generate a random Individual
      * 
@@ -39,17 +39,21 @@ public class Individual implements Runnable {
         //the 1st, 3rd, 4th, 5th heuristics should be positive
         //the 2nd should be negative
         //WE DON'T TRAIN isLost() heuristic in GA
-        weight[0] = (Math.random()) * 100;
-        weight[2] = (Math.random()) * 100;
-        weight[3] = (Math.random()) * 100;
-        weight[4] = (Math.random()) * 100;
-        weight[1] = (Math.random()) * (-100);
-        weight[5] = 10000;
+        double value;
+        for (int i = 0; i < size-1; i++) {
+            if (i == 1) {
+                value = Math.random() * (-50);
+            } else {
+                value = Math.random() * 50;
+            }
+            weight[i] = value;
+        }
+        weight[size-1] = 5000;
         player = new PlayerSkeleton();
         player.setWeightVector(weight);
         return random;
     }
-    
+
     /**
      * Change the value of the heuristic weight
      * 
@@ -59,7 +63,7 @@ public class Individual implements Runnable {
     public void setWeight(int i, double value){
         weight[i] = value;
     }
-    
+
     /**
      * Get the value of the heuristic weight
      * 
@@ -69,7 +73,7 @@ public class Individual implements Runnable {
     public double getWeight(int i){
         return weight[i];
     }
-    
+
     /**
      * If no game has been run, run it
      * Or else, just ignore and return the fitness of this individual
@@ -84,7 +88,7 @@ public class Individual implements Runnable {
         } 
         return fitness;
     }
-    
+
     /**
      * Reset the game for this Individual, after 1 generation is done
      */
@@ -98,5 +102,5 @@ public class Individual implements Runnable {
     public void run() {
         getFitness();
     }
-    
+
 }
